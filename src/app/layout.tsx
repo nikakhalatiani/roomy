f20f5317e8003fb569fd7202c4e6b7ee17571140
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2024-06-30 03:09:54 +0400

    crated landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={cn("relative h-full antialiased", inter.className)}
+        className={cn("relative h-full text-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Navbar/>
           <div className="flex-grow flex-1">{children}</div>
         </main>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,73 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { CircleUserRound, SearchCheck, MessageCircle } from "lucide-react";
+import Link from "next/link";
+
+const features = [
+  {
+    name: "Create a Profile",
+    Icon: CircleUserRound,
+    description: "Let others know you",
+  },
+  {
+    name: "Find a Roommate",
+    Icon: SearchCheck,
+    description: "Look for people who share same interests as you",
+  },
+  {
+    name: "Chat in Real Time",
+    Icon: MessageCircle,
+    description: "Get to know each other better with our chat feature",
+  },
+];
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Welcome to <span className="text-rose-600">Roomy</span>!
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Find a roommate who shares your lifestyle{" "}
+          </p>
+          <div className="'flex flex-col sm:flex-row gap-4 mt-6">
+            <Link
+              href="/people"
+              className={buttonVariants({ variant: "default", size: "lg" })}
+            >
+              <span className="text-base">Look for People</span>
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gp-x-8 lg:gap-y-0">
+            {features.map((feature) => (
+              <div
+                key={feature.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-14 w-14 flex items-center justify-center rounded-full bg-rose-100 text-rose-700">
+                    {" "}
+                    {<feature.Icon className="w-1/2 h-1/2" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="test-base font-medium text-gray-900">
+                    {feature.name}
+                  </h3>
+                  <p className="mt-3 test-sm text-muted-foreground">
+                    {feature.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
   );
 }
