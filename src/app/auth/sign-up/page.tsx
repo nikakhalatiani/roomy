ab415030e50e9bc3f3777a992c22ba832c5baaf0
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2024-07-04 11:00:19 +0400

    got rid of fragments

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -19,7 +19,6 @@ const Page = () => {
   }
 
   return (
-    <>
       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
@@ -83,7 +82,6 @@ const Page = () => {
           </div>
         </div>
       </div>
-    </>
   );
 };
 
